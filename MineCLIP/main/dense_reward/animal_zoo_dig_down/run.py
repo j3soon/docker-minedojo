091c2d574dedf61b94c8af2f79e8c579dae88dc5
Johnson Sun <j3.soon777@gmail.com>
2024-10-23 01:36:35 +0800

    Add dummy dig down script

--- MineCLIP/main/dense_reward/animal_zoo_dig_down/run.py
+++ MineCLIP/main/dense_reward/animal_zoo_dig_down/run.py
@@ -1,3 +1,7 @@
+import torch
+import torchvision
+import numpy as np
+
 from tqdm import tqdm
 from mineclip import HuntCowDenseRewardEnv
 
@@ -11,12 +15,20 @@ if __name__ == "__main__":
     )
 
     for i in tqdm(range(2), desc="Episode"):
+        frames = []
         obs = env.reset()
         done = False
         pbar = tqdm(desc="Step")
         while not done:
             action = env.action_space.no_op()
+            action[3] = 24 # change camera pitch by 180 degree
+            action[5] = 3  # action "attack"
             obs, reward, done, info = env.step(action)
+            frames.append(obs["rgb"])
             pbar.update(1)
         print(f"{i+1}-th episode ran successful!")
+        frames = np.stack(frames)
+        frames = torch.from_numpy(frames)
+        frames = frames.permute(0,2,3,1)
+        torchvision.io.write_video(f"/workspace/video{i}.mp4", frames, fps=24)
     env.close()
